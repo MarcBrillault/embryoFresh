53add4fa409ca8a97107385a6fe0a587cc122b50
Marc Brillault <marc@brillault.fr>
2018-11-17 00:24:57 +0100

    Robots.txt

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('robots.txt', 'RobotsController@index');
+
 Route::get('adminImage/{object}/{size}/{id}', 'ImageController@adminImage')
     ->where('size', 'thumbnail|preview')
     ->name('admin_image');
